783994ac863ab43578d4e0267241337a4511f542
hperez29 <perezherbie29@yahoo.com>
2025-04-23 15:53:28 -0400

    update demos3

--- wb3demos/src/main/java/com/pluralsight/Main.java
+++ wb3demos/src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,31 @@
 package com.pluralsight;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        FileInputStream fis;
+        Scanner fileScanner = null;
+
+        try {
+            fis = new FileInputStream("jokes.txt");
+            fileScanner = new Scanner(fis);
+
+            while (fileScanner.hasNextLine()) {
+                String currentLine = fileScanner.nextLine();
+                System.out.println(currentLine);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: The file 'jokes.txt' was not found.");
+        } finally {
+            if (fileScanner != null) {
+                fileScanner.close();
+            }
+        }
     }
 }
